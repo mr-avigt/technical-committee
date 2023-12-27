bd8fb6043566adc15d7f74206c0a8613db5b38f8
Abhijeet anand <115696107+mr-avigt@users.noreply.github.com>
2023-12-28 00:51:32 +0530

    Add files via upload

---
+++ Combined/jind.js
@@ -0,0 +1,50 @@
+const cardBunch = document.querySelectorAll(".card__inner");
+
+
+cardBunch.forEach(function (card){
+  card.addEventListener("click", function (e) {
+    card.classList.toggle('is-flipped');
+  });
+})
+
+
+
+// 2nd---page
+
+var tl = gsap.timeline({scrollTrigger:{
+    trigger:"#main-gallery",
+   //  markers:true,
+    start:"50% 50%",
+    end:"150% 50%",
+    scrub:2,
+    pin:true
+}});
+tl
+.to("#center-gallery",{
+   height: "100vh",
+},'a')
+.to("#top-gallery",{
+    top: "-50%",
+ },'a')
+ .to("#bottom-gallery",{
+    bottom: "-50%",
+ },'a')
+.to("#top-h1",{
+    top: "60%"
+ },'a')
+ .to("#bottom-h1",{
+    bottom: "-30%"
+ },'a')
+.to("#center-h1",{
+   top: "-30%"
+},'a')
+.to(".content-gallery",{
+   delay: -0.2,
+   marginTop: "0%"
+})
+
+
+// 3rd Page
+
+
+
