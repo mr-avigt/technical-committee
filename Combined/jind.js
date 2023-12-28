74c2a4814f1d3cf60129833d8866829c3c08b762
DarthVader001 <122208414+DarthVader001@users.noreply.github.com>
2023-12-28 20:17:54 +0530

    Update jind.js

--- Combined/jind.js
+++ Combined/jind.js
@@ -117,3 +117,29 @@ $('#countdown').countdown({
 	}
 });
 
+// homepage
+const button = document.querySelector(".button");
+const mobileMenu = document.querySelector(".mobile-menu");
+var elements = document.querySelectorAll(".mobile_nav_elements");
+let isMenuOpen = false;  // Flag to track menu state
+
+function toggleMenu() {
+  if (isMenuOpen) {
+    // If menu is open, force hide it without animation
+    mobileMenu.classList.toggle("show-out");
+    mobileMenu.style.transform = "translateX(-110%)";  // Set initial position
+    mobileMenu.style.opacity = "0";
+    isMenuOpen = false;
+  } else {
+    // If menu is closed, toggle the "show" class for animation
+    mobileMenu.classList.toggle("show");
+    isMenuOpen = true;
+  }
+}
+
+button.addEventListener("click", toggleMenu);
+elements.forEach(element => {
+  element.addEventListener("click" ,toggleMenu );
+});
+
+
